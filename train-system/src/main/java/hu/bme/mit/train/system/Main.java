eca90719e79d04348e4a4f05b8c8fbc44030b14c
comLlama <alphalupus846@gmail.com>
2021-03-29 21:30:15 +0200

    Changes in change request implemented

--- train-system/src/main/java/hu/bme/mit/train/system/Main.java
+++ train-system/src/main/java/hu/bme/mit/train/system/Main.java
@@ -8,18 +8,30 @@ public class Main {
 	public static void main (String[] args) {
 		TrainSystem trainSystem = new TrainSystem();
 		Timer timer = new Timer();
+
+		int interval;
+		Scanner console = new Scanner(System.in);
+		System.out.println("Choose an update interval in seconds:");
+		try {
+			interval = 1000 * Integer.parseInt(console.nextLine());
+		} catch (NumberFormatException e){
+			interval = 5000;
+		}
 		timer.schedule(new TimerTask() {
 			@Override
 			public void run() {
 				trainSystem.getController().followSpeed();
 			}
-		}, 1000, 5000);
+		}, 1000, interval);
 
 		String line;
-		Scanner console = new Scanner(System.in);
 		while (!(line = console.nextLine()).equals("q")){
-			int joyInput = Integer.parseInt(line);
-			trainSystem.getUser().overrideJoystickPosition(joyInput);
+			try {
+				int joyInput = Integer.parseInt(line);
+				trainSystem.getUser().overrideJoystickPosition(joyInput);
+			} catch (NumberFormatException e) {
+				System.out.println("NaN");
+			}
 		}
 
 		timer.cancel();
